802dd3db1782f904908bfb6f46f6408d13977dd0
Tuane Quintella <q.tuane@gmail.com>
2015-07-05 15:41:54 -0300

    Favorite button animation (and trying to load show screenshot with Glide)

--- SeriesTracker/app/src/main/java/com/movile/next/seriestracker/activity/ShowDetailsActivity.java
+++ SeriesTracker/app/src/main/java/com/movile/next/seriestracker/activity/ShowDetailsActivity.java
@@ -1,5 +1,7 @@
 package com.movile.next.seriestracker.activity;
 
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.view.ViewPager;
@@ -9,9 +11,11 @@ import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.movile.next.seriestracker.R;
+import com.movile.next.seriestracker.activity.base.BaseNavigationDrawerActivity;
 import com.movile.next.seriestracker.activity.base.BaseNavigationToolbarActivity;
 import com.movile.next.seriestracker.adapter.ShowDetailsPagerAdapter;
 import com.movile.next.seriestracker.listener.OnSeasonClickListener;
+import com.movile.next.seriestracker.model.Images;
 import com.movile.next.seriestracker.model.Season;
 import com.movile.next.seriestracker.model.Show;
 import com.movile.next.seriestracker.presenter.ShowDetailsPresenter;
@@ -51,7 +55,10 @@ public class ShowDetailsActivity extends BaseNavigationToolbarActivity implement
         hideLoading();
         getSupportActionBar().setTitle(show.title());
         ImageView showScreenshot = (ImageView) findViewById(R.id.show_screenshot);
-        //Glide.with(this).load(show.images().poster()).centerCrop().into(showScreenshot);
+        Glide.with(this).load(show.images().banner().get(Images.ImageSize.MEDIUM))
+                .placeholder(R.drawable.show_item_placeholder)
+                .centerCrop()
+                .into(showScreenshot);
 
         mShow = show;
         mContentPager = (ViewPager) findViewById(R.id.show_details_content);
@@ -68,21 +75,33 @@ public class ShowDetailsActivity extends BaseNavigationToolbarActivity implement
 
     @Override
     public void setFavorite(boolean isFav) {
-        FloatingActionButton favoriteView = (FloatingActionButton) findViewById(R.id.show_details_favorite_button);
         if(isFav) {
-            favoriteView.setImageResource(R.drawable.show_details_favorite_on);
-            favoriteView.setBackgroundTintList(getResources().getColorStateList(R.color.default_color_second));
+            mFavoriteView.setImageResource(R.drawable.show_details_favorite_on);
+            mFavoriteView.setBackgroundTintList(getResources().getColorStateList(R.color.default_color_second));
         } else {
-            favoriteView.setImageResource(R.drawable.show_details_favorite_off);
-            favoriteView.setBackgroundTintList(getResources().getColorStateList(R.color.default_color_third));
+            mFavoriteView.setImageResource(R.drawable.show_details_favorite_off);
+            mFavoriteView.setBackgroundTintList(getResources().getColorStateList(R.color.default_color_third));
         }
     }
 
     private void configureFavoriteButton() {
         mFavoriteView = (FloatingActionButton) findViewById(R.id.show_details_favorite_button);
+        ObjectAnimator animationX1 = ObjectAnimator.ofFloat(mFavoriteView, "scaleX", 1, 0.6f);
+        ObjectAnimator animationY1 = ObjectAnimator.ofFloat(mFavoriteView, "scaleY", 1, 0.6f);
+
+        ObjectAnimator animationX2 = ObjectAnimator.ofFloat(mFavoriteView, "scaleX", 0.6f, 1);
+        ObjectAnimator animationY2 = ObjectAnimator.ofFloat(mFavoriteView, "scaleY", 0.6f, 1);
+
+        final AnimatorSet animationSet = new AnimatorSet();
+        animationSet.playTogether(animationX1, animationY1);
+        animationSet.playTogether(animationX2, animationY2);
+        animationSet.playSequentially(animationX1, animationX2);
+        animationSet.setDuration(100);
+
         mFavoriteView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                animationSet.start();
                 mPresenter.toggleFavoriteStatus(mShow);
             }
         });
