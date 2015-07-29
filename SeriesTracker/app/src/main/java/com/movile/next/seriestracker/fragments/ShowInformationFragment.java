0a3f0537148deb0faa909652bac3e565c21cba1e
Tuane Quintella <q.tuane@gmail.com>
2015-07-29 17:56:01 -0300

    Show information tab slider with some organizational refactoring

--- SeriesTracker/app/src/main/java/com/movile/next/seriestracker/fragments/ShowInformationFragment.java
+++ SeriesTracker/app/src/main/java/com/movile/next/seriestracker/fragments/ShowInformationFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.movile.next.seriestracker.R;
 import com.movile.next.seriestracker.model.Show;
@@ -13,6 +14,10 @@ public class ShowInformationFragment extends Fragment {
 
     private Show mShow;
 
+    public ShowInformationFragment() {
+        //Empty constructor required
+    }
+
     public ShowInformationFragment(Show show) {
         mShow = show;
     }
@@ -20,13 +25,37 @@ public class ShowInformationFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        setShowDetailsInfo();
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.show_information_fragment, container, false);
+        View view = inflater.inflate(R.layout.show_information_fragment, container, false);
+        setShowDetailsInfo(view);
+        return view;
+
     }
 
-    private void setShowDetailsInfo() {
-        //TODO: set show details info on TextViews
+    private void setShowDetailsInfo(View view) {
+        TextView textView = (TextView) view.findViewById(R.id.show_summary_text);
+        textView.setText(mShow.overview());
+
+        textView = (TextView) view.findViewById(R.id.show_genre_text);
+        textView.setText(mShow.genres()[0]);
+
+        textView = (TextView) view.findViewById(R.id.show_network_text);
+        textView.setText(mShow.network());
+
+        textView = (TextView) view.findViewById(R.id.show_status_text);
+        textView.setText(mShow.status());
+
+        textView = (TextView) view.findViewById(R.id.show_episodes_text);
+        textView.setText(String.valueOf(mShow.airedEpisodes()));
+
+        textView = (TextView) view.findViewById(R.id.show_year_text);
+        textView.setText(String.valueOf(mShow.year()));
+
+        textView = (TextView) view.findViewById(R.id.show_country_text);
+        textView.setText(mShow.country().toUpperCase());
+
+        textView = (TextView) view.findViewById(R.id.show_language_text);
+        textView.setText(mShow.language());
+
     }
 
 }
